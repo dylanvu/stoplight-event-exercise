faaa2c942b9bad308760091678938d96d069b7da
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-10 15:46:27 -0800

    add button functionality to change lights

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-/*
-  Write JS to make this stoplight work.
+(function() {
+  'use strict';
 
-  When I click on the 'stop' button,
-    the top light should turn red.
-  When I click on the 'slow' button
-    the middle light should turn orange.
-  When I click on the 'go' button
-    the bottom light should turn green.
-*/
+  const stopButton = document.querySelector('#stopButton');
+  const slowButton = document.querySelector('#slowButton');
+  const goButton = document.querySelector('#goButton');
+
+  const stopLight = document.querySelector('#stopLight');
+  const slowLight = document.querySelector('#slowLight');
+  const goLight = document.querySelector('#goLight');
+
+  const createLightEventListener = (button, light, color) => {
+    button.addEventListener('click', () => {
+    stopLight.style.backgroundColor = 'black';
+    slowLight.style.backgroundColor = 'black';
+    goLight.style.backgroundColor = 'black';
+    light.style.backgroundColor = color;
+  })}
+
+  createLightEventListener(stopButton, stopLight, 'red');
+  createLightEventListener(slowButton, slowLight, 'orange');
+  createLightEventListener(goButton, goLight, 'lightgreen');
+})();

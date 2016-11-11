d9b5fc21ee1ec9ed277e822b06297945330a7e2a
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-11 10:13:36 -0800

    refactor to seperate functions

--- script.js
+++ script.js
@@ -9,29 +9,43 @@
   const slowLight = document.querySelector('#slowLight');
   const goLight = document.querySelector('#goLight');
 
-  const createButtonEvents = (button, light, className) => {
+  const mouseoverEvent = (button) => {
     button.addEventListener('mouseover', () => {
       console.log(`Entered ${button.textContent} button`);
     });
+  }
+
+  const mouseoutEvent = (button) => {
     button.addEventListener('mouseout', () => {
       console.log(`Left ${button.textContent} button`);
     });
+  }
+
+  const clickEvent = (button, light, className) => {
     button.addEventListener('click', () => {
       console.log(button.textContent);
       stopLight.classList.toggle('stop', false);
       slowLight.classList.toggle('slow', false);
       goLight.classList.toggle('go', false);
       light.classList.toggle(className);
-      // button.addEventListener('click', () => {
-      //   if (light.classList.contains(className)) {
-      //     light.classList.toggle(className);
-      //   }
-      //   else {
-      //     light.classList.toggle(className);
-      //   }
-      // });
     });
+  }
+
+  const createButtonEvents = (button, light, className) => {
+    mouseoverEvent(button);
+    mouseoutEvent(button);
+    clickEvent(button, light, className);
   };
+
+  // Toggle on and off
+  // button.addEventListener('click', () => {
+  //   if (light.classList.contains(className)) {
+  //     light.classList.toggle(className);
+  //   }
+  //   else {
+  //     light.classList.toggle(className);
+  //   }
+  // });
 
   createButtonEvents(stopButton, stopLight, 'stop');
   createButtonEvents(slowButton, slowLight, 'slow');
